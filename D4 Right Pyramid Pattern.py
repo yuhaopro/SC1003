9b537744003a9e89f80283a03fcf810dd5dc6536
yuhaopro <64051449+yuhaopro@users.noreply.github.com>
2022-09-03 14:13:37 +0800

    Delete D4 Right Pyramid Pattern.py

--- D4 Right Pyramid Pattern.py
+++
@@ -1,9 +0,0 @@
-_userinput = int(input("Please enter pattern width: "))
-for i in range(_userinput):
-    for j in range(i + 1):
-        print("*", end = "")
-    print("\n")
-for p in range(_userinput - 1, 0, -1):
-    for q in range(p):
-        print("*", end = "")
-    print("\n")
