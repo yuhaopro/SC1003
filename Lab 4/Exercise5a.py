dc72abd106da1b32cce548940b9df0b84be2eef3
yuhaopro <yuhao9583@gmail.com>
2022-09-06 14:38:11 +0800

    Added Lab 4 Ex5a

---
+++ Lab 4/Exercise5a.py
@@ -0,0 +1,20 @@
+#Define 4 colours
+#Display 4 random colours on all 4 corners of the screen
+#Width of RPI screen = 0 - 7
+#Height of RPI screen = 0 - 7
+# Display at position 0-0, 0-7, 7-0, 7-7
+from random import randint
+_RPISCREENWIDTH = 8
+_RPISCREENHEIGHT = 8
+red = (255,0,0)
+yellow = (255,255,0)
+blue = (0,0,255)
+green = (0,255,0)
+
+colorList = [red, yellow, blue, green]
+
+for i in range(_RPISCREENHEIGHT):
+    for j in range(_RPISCREENWIDTH):
+        if i == 0 and j == 0:
+            #sense.set_pixel(i,j,colorList[randint(0, len(colorList) - 1)])
+            pass
