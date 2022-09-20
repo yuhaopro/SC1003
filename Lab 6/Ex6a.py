d2c1fd0816fb40bf0f6381ea60d34519f910bf48
yuhaopro <yuhao9583@gmail.com>
2022-09-20 10:32:47 +0800

    Finished Lab 6

--- Lab 6/Ex6a.py
+++ Lab 6/Ex6a.py
@@ -1,16 +1,13 @@
+from Ex6b import move_marble
 from sense_hat import SenseHat
 from time import sleep
 
 sense = SenseHat()
+sense.clear()
 
 while True:
     pitch = sense.get_orientation()['pitch'] 
     roll = sense.get_orientation()['roll'] 
-    pitch = round(pitch, 0)
-    roll = round(roll, 0)
-    x,y = move_marbla(pitch,roll,x,y)
-    board[y][x] = w
+    print("pitch {0} roll {1}".format(round(pitch,0), round(roll,0)))
+sleep(0.05)
 
-    #print("pitch {0} roll {1}".format(round(pitch,0), round(roll,0))
-    print('pitch: ',pitch, 'roll: ', roll)
-    sleep(0.05)
--- Lab 6/Ex6d.py
+++ Lab 6/Ex6d.py
@@ -5,6 +5,9 @@ from time import sleep
 # to determine whether to move the marble in the x-direction.
 # Similarly, it checks the roll value and y coordinate to
 # determine whether to move the marble in the y-direction.
+
+
+
 def move_marble(pitch,roll,x,y): 
     new_x = x    #assume no change to start with
     new_y = y    #assume no change to start with
@@ -25,15 +28,44 @@ x = 2
 y = 2
 b = [0,0,0]
 w = [255,255,255]
+r = [255,0,0]
 #2d array
-board = [[b,b,b,b,b,b,b,b], 
+board = [[r,r,r,b,b,b,b,r], 
+         [r,b,b,b,b,b,b,r],
+         [b,b,b,b,b,r,b,r],
+         [b,r,r,b,r,r,b,r],
          [b,b,b,b,b,b,b,b],
-         [b,b,b,b,b,b,b,b],
-         [b,b,b,b,b,b,b,b],
-         [b,b,b,b,b,b,b,b],
-         [b,b,b,b,b,b,b,b],
-         [b,b,b,b,b,b,b,b], 
-         [b,b,b,b,b,b,b,b]]
+         [b,r,b,r,r,b,b,b],
+         [b,b,b,r,b,b,b,r], 
+         [r,r,b,b,b,r,r,r] ]
+
+
+def check_wall(x,y,new_x,new_y): 
+    if board[new_y][new_x] != r: 
+        return new_x, new_y 
+    elif board[new_y][x] != r: 
+        return x, new_y 
+    elif board[y][new_x] != r:
+        return new_x, y 
+    else:
+        return x,y
+
+def move_marble(pitch,roll,x,y): 
+    new_x = x    #assume no change to start with
+    new_y = y    #assume no change to start with
+    if 1 < pitch < 179 and x != 0: 
+        new_x -= 1    # move left
+    elif 359 > pitch > 179 and x != 7: 
+        new_x += 1    # move right
+    if 1 < roll < 179 and y != 7: 
+        new_y += 1    # move up
+    elif 359 > roll > 179 and y != 0: 
+        new_y -= 1    # move down
+    new_x,new_y = check_wall(x,y,new_x,new_y) 
+    return new_x, new_y
+
+
+
 board_1D=sum(board,[])        # convert to 1-dimension list
 print(board_1D)               # for code debugging
 sense.set_pixels(board_1D)    # display it
